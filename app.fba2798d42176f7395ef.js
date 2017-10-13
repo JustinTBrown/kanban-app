368286c19b793160cdf92f7f5c83785119aee944
Justin Brown <brown.jt@gmail.com>
2017-10-12 19:03:48 -0600

    Updates

---
+++ app.fba2798d42176f7395ef.js
@@ -0,0 +1,2 @@
+webpackJsonp([0,3],[function(e,t,l){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}var u=l(1),d=n(u),r=l(31),a=n(r);a.default.render(d.default.createElement("div",null,"Hello world, this is great!"),document.getElementById("app"))}]);
+//# sourceMappingURL=app.fba2798d42176f7395ef.js.map
